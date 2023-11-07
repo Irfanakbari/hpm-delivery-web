9d5f983c3aebe10c9989e39a39da8a4b1549fa96
Irfan Akbari Habibi <irfanakbarihabibi18@gmail.com>
2023-11-07 13:23:25 +0700

    bug gara gara tanda petik di halaman block

--- src/app/(block)/block/page.tsx
+++ src/app/(block)/block/page.tsx
@@ -17,7 +17,7 @@ export default function Block(){
       <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-800 to-black">
         <h1 className="text-9xl text-white text-opacity-25 font-montserrat fixed w-full text-center z-10 transform -translate-y-1/2 top-1/2">403</h1>
         <div className="bg-transparent w-full md:w-full lg:w-3/4 p-8 rounded-lg shadow-xl z-20">
-          <p className="font-mono text-gray-200 text-lg mb-6">&gt; <span className="text-yellow-400">ERROR CODE</span>: "
+          <p className="font-mono text-gray-200 text-lg mb-6">&gt; <span className="text-yellow-400">ERROR CODE</span>: &quot;
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
@@ -28,8 +28,8 @@ export default function Block(){
               wrapper="span"
               speed={40}
               style={{ display: 'inline-block', fontStyle: 'italic' }}
-            />"</p>
-          <p className="font-mono text-gray-200 text-lg mb-6">&gt; <span className="text-yellow-400">ERROR DESCRIPTION</span>: "
+            />&quot;</p>
+          <p className="font-mono text-gray-200 text-lg mb-6">&gt; <span className="text-yellow-400">ERROR DESCRIPTION</span>: &quot;
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
@@ -39,8 +39,8 @@ export default function Block(){
               speed={40}
               style={{ display: 'inline-block', fontStyle: 'italic' }}
             />
-            "</p>
-          <p className="font-mono text-gray-200 text-lg mb-6">&gt; <span className="text-yellow-400">ERROR POSSIBLY CAUSED BY</span>: [<b>You don't have the appropriate role to access this application, or you don't have permission to access it, please contact super admin to change the permissions on your account.</b>...]</p>
+              &quot;</p>
+          <p className="font-mono text-gray-200 text-lg mb-6">&gt; <span className="text-yellow-400">ERROR POSSIBLY CAUSED BY</span>: [<b>You don&apos;t have the appropriate role to access this application, or you don&apos;t have permission to access it, please contact super admin to change the permissions on your account.</b>...]</p>
           {/*<p className="font-mono text-gray-200 text-lg mb-6">&gt; <span className="text-yellow-400">SOME PAGES ON THIS SERVER THAT YOU DO HAVE PERMISSION TO ACCESS</span>: [<a href="/" className="text-blue-500">Home Page</a>, <a href="/" className="text-blue-500">About Us</a>, <a href="/" className="text-blue-500">Contact Us</a>, <a href="/" className="text-blue-500">Blog</a>...]</p>*/}
           <p className="font-mono text-gray-200 text-lg">&gt; <span className="text-yellow-400">HAVE A NICE DAY {session?.user?.name?.toUpperCase()} :-)</span></p>
         </div>
